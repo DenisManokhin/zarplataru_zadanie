4c067e12d13c467e8529f5d9e4d33264436fd28d
DenisManokhin <31123029+DenisManokhin@users.noreply.github.com>
2017-09-03 12:26:11 +0700

    upd

--- src/actions/index.js
+++ src/actions/index.js
@@ -12,9 +12,9 @@ import {
 export const getVacTable = () => dispatch => {
     axios.get('https://api.zp.ru/v1/vacancies/?average_salary=true&categories_facets=true&geo_id=826&highlight=true&period=today&search_type=fullThrottle&state=1')
     .then( response => {
-        dispatch( { type: GET_VAC_COUNT, payload:  response.data.metadata.resultset.count  } );
-        dispatch( { type: GET_VAC_TABLE, payload:  response.data.metadata.categories_facets  } );
-        dispatch( { type: IS_FETCH_VAC_TABLE, payload:  false } );
+        dispatch( { type: GET_VAC_COUNT, payload: response.data.metadata.resultset.count } );
+        dispatch( { type: GET_VAC_TABLE, payload: response.data.metadata.categories_facets } );
+        dispatch( { type: IS_FETCH_VAC_TABLE, payload: false } );
     } )
 };
 
@@ -22,6 +22,6 @@ export const getWordsTable = () => dispatch => {
     axios.get('https://api.zp.ru/v1/vacancies?geo_id=826&headers_facets=1&period=today')
     .then( response => {
         dispatch( { type: GET_WORDS_TABLE, payload: response.data.metadata.headers_facets } );
-        dispatch( { type: IS_FETCH_WORDS_TABLE, payload:  false } );
+        dispatch( { type: IS_FETCH_WORDS_TABLE, payload: false } );
     } )
 };
